8b139406758da3d548e5c7d9af9f6dd0d2427889
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-02 15:11:47 +0000

    day 2

--- day02-2.rb
+++ day02-2.rb
@@ -2,24 +2,23 @@
 
 input = File.read(File.expand_path("../day02.txt", __FILE__))
 
+def invalid_id(s)
+  return false if s.length < 2
+  (1...s.length).each do |i|
+    s1 = s[0, i]
+    s2 = s[i..]
+    # TODO make less meh.
+    (0..s.length).each { s2.chomp!(s1) }
+    if s2.length == 0
+      return true
+    end
+  end
+  false
+end
+
 sum_invalid_ids = input.split(",").flat_map { |r|
   n, m = r.split("-").map(&:to_i)
-  (n..m).to_a
-        .filter { _1 > 9 }
-        .filter {
-          s = _1.to_s
-          is_funny_id = false
-          (1...s.length).each do |i|
-            s1 = s[0, i]
-            s2 = s[i..]
-            (0..s.length).each { s2.chomp!(s1) }
-            if s2.length == 0
-              is_funny_id = true
-              break
-            end
-          end
-          is_funny_id
-        }
+  (n..m).to_a.filter { invalid_id _1.to_s }
 }.sum
 
 puts "solution part 2: #{sum_invalid_ids}"
