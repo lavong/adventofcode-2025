fc54ef93c49a3dcacaec2abe92d72697a19a89e3
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-05 07:24:29 +0100

    day 5

--- day05-2.rb
+++ day05-2.rb
@@ -4,14 +4,11 @@ input = File.read(File.expand_path("../day05.txt", __FILE__))
 
 ranges = input.split(/\n{2,}/)[0].split("\n").map { |line| line.split("-").map(&:to_i).to_a }.sort!
 
-fresh_ids = 0
 p = 0
-for n, m in ranges
-  start = p >= n ? p + 1 : n
-  p = [p, m].max
-  if start <= m
-    fresh_ids += m - start + 1
-  end
-end
+fresh_ids = ranges.map { |s, e|
+  start = p >= s ? p + 1 : s
+  p = [p, e].max
+  start <= e ? e - start + 1 : 0
+}.sum
 
 puts "solution part 2: #{fresh_ids}"
