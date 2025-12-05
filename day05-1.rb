ae6201328e22e337c9cd8e615834ddf001804c78
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-05 07:31:49 +0100

    day 5

--- day05-1.rb
+++ day05-1.rb
@@ -4,19 +4,7 @@ input = File.read(File.expand_path("../day05.txt", __FILE__))
 
 ranges, ids = input.split(/\n{2,}/).map { |blocks| blocks.split("\n") }
 ranges = ranges.map { |r| r.split('-').map(&:to_i) }
-ids = ids.map(&:to_i)
 
-fresh_ingredients = 0
-for id in ids
-  fresh = false
-  for n, m in ranges
-    if id >= n && id <= m
-      fresh = true
-    end
-  end
-  if fresh
-    fresh_ingredients +=1
-  end
-end
+fresh_ingredients = ids.map(&:to_i).map { |id| ranges.any? { |n, m| (n..m).include? id } ? 1 : 0 }.sum
 
 puts "solution part 1: #{fresh_ingredients}"
