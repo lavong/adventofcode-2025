5fb4edefe22015752d16c8e5ed337dc89658cb58
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-06 06:32:20 +0100

    day 6

---
+++ day06-1.rb
@@ -0,0 +1,19 @@
+#!/usr/bin/env ruby
+
+input = File.read(File.expand_path("../day06.txt", __FILE__))
+
+lines = input.split("\n").map { |line| line.split("\s") }.transpose
+
+grand_total = 0
+lines.each do |line|
+  op = line.pop
+  nums = line.map { |n| n.to_i }
+  case op
+  when '*'
+    grand_total += nums.reduce(:*)
+  else
+    grand_total += nums.sum
+  end
+end
+
+puts "solution part 1: #{grand_total}"
