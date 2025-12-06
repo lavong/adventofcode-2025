099bcf895b6faf472bbe338d8646ca62dee276fb
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-06 08:21:07 +0100

    day 6

--- day06-2.rb
+++ day06-2.rb
@@ -6,30 +6,20 @@ grid = input.split("\n").map { |line| line.chars }
 
 grand_total = 0
 op = grid[-1][0]
-block_x = 0
-for x in 0..grid[0].length
+block_start_x = 0
+(0..grid[0].length).each { |x|
   op = grid[-1][x] if %w[* +].include? grid[-1][x]
-
-  block_end = true
-  for y in 0...grid.length
-    block_end = false if x < grid[y].length && grid[y][x] != ' '
-  end
+  block_end = (0...grid.length).none? { |y| x < grid[y].length && grid[y][x] != ' ' }
 
   if block_end
     total = op == '*' ? 1 : 0
-    for dx in block_x...x
-      n = 0
-      for y in 0...(grid.length - 1)
-        dn = grid[y][dx]
-        if dn != ' '
-          n = 10 * n + dn.to_i
-        end
-      end
+    (block_start_x...x).each { |dx|
+      n = (0...(grid.length - 1)).map { grid[_1][dx] }.select { _1 != ' ' }.join.to_i
       op == '*' ? total *= n : total += n
-    end
-    block_x = x + 1
+    }
+    block_start_x = x + 1
     grand_total += total
   end
-end
+}
 
 puts "solution part 2: #{grand_total}"
