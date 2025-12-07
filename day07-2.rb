cbb1c61290f3a77b083e58e20c151b6497e977cd
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-07 08:32:11 +0100

    day 7

--- day07-2.rb
+++ day07-2.rb
@@ -2,17 +2,18 @@
 
 input = File.read(File.expand_path("../day07.txt", __FILE__))
 
-def timelines(y, x, grid, cache)
+def timelines(y, x, grid)
+  @cache ||= {}
   return 1 if y + 1 >= grid.length
-  return cache[[y, x]] if cache.include? [y, x]
+  return @cache[[y, x]] if @cache.include? [y, x]
   if grid[y + 1][x] == '^'
-    cache[[y, x]] = timelines(y + 1, x - 1, grid, cache) + timelines(y + 1, x + 1, grid, cache)
+    @cache[[y, x]] ||= timelines(y + 1, x - 1, grid) + timelines(y + 1, x + 1, grid)
   else
-    cache[[y, x]] = timelines(y + 1, x, grid, cache)
+    @cache[[y, x]] ||= timelines(y + 1, x, grid)
   end
 end
 
 grid = input.split("\n").map { |line| line.chars }
-timelines = timelines 0, input.index('S'), grid, {}
+timelines = timelines 0, input.index('S'), grid
 
 puts "solution part 2: #{timelines}"
