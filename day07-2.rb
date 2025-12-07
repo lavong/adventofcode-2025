9abe980b1e98e4eabb108ef86abecc431e45fdd5
Lavong Soysavanh <lavong.soysavanh@gmail.com>
2025-12-07 07:55:51 +0100

    day 7

--- day07-2.rb
+++ day07-2.rb
@@ -2,15 +2,6 @@
 
 input = File.read(File.expand_path("../day07.txt", __FILE__))
 
-def find(s, grid)
-  grid.each_with_index do |_, y|
-    grid[y].each_with_index do |c, x|
-      return [y, x] if c == s
-    end
-  end
-  [0, 0]
-end
-
 def timelines(y, x, grid, cache)
   return 1 if y + 1 >= grid.length
   return cache[[y, x]] if cache.include? [y, x]
@@ -22,7 +13,6 @@ def timelines(y, x, grid, cache)
 end
 
 grid = input.split("\n").map { |line| line.chars }
-sy, sx = find 'S', grid
-timelines = timelines sy, sx, grid, {}
+timelines = timelines 0, input.index('S'), grid, {}
 
 puts "solution part 2: #{timelines}"
